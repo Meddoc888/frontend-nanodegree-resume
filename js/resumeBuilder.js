42015e49d68ffc24b61c5e3e5985fb890b4f9696
Meddoc888 <Meddoc888@users.noreply.github.com>
2017-06-03 09:47:36 -0400

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,6 @@
- $("#main").append("Ritu");
-var awesomeThoughts = "I am Ritu and I am awesome!";
-console.log(awesomeThoughts);
-var newAwesomeThoughts = awesomeThoughts.replace("awesome", "Fun");
-console.log(newAwesomeThoughts);
-$("#main").append(newAwesomeThoughts);
+var formattedName = HTMLheaderName.replace("%data%", "Ritu");
+var formattedRole = HTMLheaderRole.replace("%data%", "Agent");
+$("#main").append(formattedName, formattedRole);
                    /*
 This is empty on purpose! Your code to build the resume will go here.
  */
