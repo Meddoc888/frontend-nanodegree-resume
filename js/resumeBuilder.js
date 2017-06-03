e40a96e868ce21da52c70c75865a742989f0fc3d
Meddoc888 <Meddoc888@users.noreply.github.com>
2017-06-03 09:16:17 -0400

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,6 @@
  $("#main").append("Ritu");
+var awesomeThoughts = "I am Ritu and I am awesome!";
+console.log(awesomeThoughts);
                    /*
 This is empty on purpose! Your code to build the resume will go here.
  */
