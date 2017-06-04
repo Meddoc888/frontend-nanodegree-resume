1797becaaf57529727289f72eba96479da8e2fb2
Meddoc888 <Meddoc888@users.noreply.github.com>
2017-06-04 13:55:54 -0400

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,13 +3,13 @@ var bio = {
   "name" : "Ritu",
   "role" : "Agent",
   "contact" : "email",
-  "Picture" : "images/fry.jpg",
-  "Message" : "Welcome to my page!",
-  "Skills" : ["Financial Reporting", "Finance", "System Implementation", "Auditing"]
+  "picture" : "images/fry.jpg",
+  "message" : "Welcome to my page!",
+  "skills" : ["Financial Reporting", "Finance", "System Implementation", "Auditing"]
 };
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role); 
-var formattedPic = HTMLbioPic.replace(
+var formattedPic = HTMLbioPic.replace("%data%", bio.picture);
 $("#main").append(formattedName, formattedRole);
                    /*
 This is empty on purpose! Your code to build the resume will go here.
